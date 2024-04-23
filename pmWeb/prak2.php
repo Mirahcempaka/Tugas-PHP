ea9fbd743eee84259600e7d0cf2157045808c036
Mirahcempaka <167783404+Mirahcempaka@users.noreply.github.com>
2024-04-23 21:24:00 +0700

    Add files via upload

---
+++ pmWeb/prak2.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h2>Nilai Mahasiswa : </h2>";
+$nim = '2301010160';
+$nama = 'Mirah Cempaka';
+$prodi = 'Teknik Informatika';
+$nilai = 90;
+echo "NIM : ", $nim, "<br>";
+echo "Nama : ", $nama, "<br>";
+echo "Prodi : ", $prodi, "<br>";
+echo "Nilai : $nilai";
+?>
---
+++ pmWeb/prak5.php
@@ -0,0 +1,9 @@
+<?php
+$username = "Mirah";
+if ($username == "Mirah") {
+echo "Anda Berhasil Login!, Selamat datang
+$username";
+} else {
+echo "Username salah, silahkan coba lagi!";
+}
+?>
